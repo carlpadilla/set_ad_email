0f6bef39661fbfc12d969a4d5d906d1fe4c04d52
Padilla <cpadilla@novakfrancella.com>
2020-08-12 10:24:38 -0400

    first commit

---
+++ setEmail.ps1
@@ -0,0 +1,7 @@
+﻿Import-Module ActiveDirectory
+$users = Get-ADUser -Filter *
+foreach ($user in $users)
+{
+    $email = $user.samaccountname + '@domain.com'
+    Set-ADUser -Identity $user.samaccountname -EmailAddress $email
+}
